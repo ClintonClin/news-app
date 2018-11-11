92b1a55b83d6fd3491724f7c7b874c4f9de0db8c
ClintonClin <clintonclin00@gmail.com>
2018-11-12 02:30:42 +0300

    made our base url in the config file

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
+import os
 class Config:
     '''
     General configuration parent class
     '''
-    pass
+    NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v1/sources?language=en&category={}'
+    NEWS_ARTICLES_BASE_URL = 'https://newsapi.org/v1/articles?source={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 
 class ProdConfig(Config):
